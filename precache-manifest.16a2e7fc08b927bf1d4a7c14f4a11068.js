075ffa85cd2bd8f5aa9fd963537c0eef45bcb5a7
Gremorie <vdwcedric@gmail.com>
2018-12-06 00:45:40 +0100

    Updates

---
+++ precache-manifest.16a2e7fc08b927bf1d4a7c14f4a11068.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "69bab70dd00f4429b781",
+    "url": "/pokedex/static/js/runtime~main.69bab70d.js"
+  },
+  {
+    "revision": "0b5c32dfdc0e94c523c5",
+    "url": "/pokedex/static/js/main.0b5c32df.chunk.js"
+  },
+  {
+    "revision": "fc6f226896c4b72657e7",
+    "url": "/pokedex/static/js/1.fc6f2268.chunk.js"
+  },
+  {
+    "revision": "0b5c32dfdc0e94c523c5",
+    "url": "/pokedex/static/css/main.fb82b64a.chunk.css"
+  },
+  {
+    "revision": "fc6f226896c4b72657e7",
+    "url": "/pokedex/static/css/1.da7c92ac.chunk.css"
+  },
+  {
+    "revision": "5964448f16bc816b89d5eff2dff8b226",
+    "url": "/pokedex/index.html"
+  }
+];
