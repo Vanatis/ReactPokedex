b66597d8635b56a66a47d82ca7e5c09bf6ec3684
Cédric Van de Walle <cvandewalle@icorda.be>
2019-01-03 18:18:20 +0100

    Added pokemon types

--- src/Components/Pokemon.js
+++ src/Components/Pokemon.js
@@ -156,6 +156,62 @@ class Pokemon extends Component {
     }
 
     let singleBodyClassName = "pokemon-single-container";
+
+    /*---------------*/
+    /*TYPE CODE*/
+    /*---------------*/
+    let typeDiv = "";
+    let pokemonTypeStyles = {};
+    if (!this.state.bPokemonDataPending && pokemonData !== null) {
+      let typeColors = {
+        normal: "#A8A77A",
+        fire: "#EE8130",
+        water: "#6390F0",
+        electric: "#F7D02C",
+        grass: "#7AC74C",
+        ice: "#96D9D6",
+        fighting: "#C22E28",
+        poison: "#A33EA1",
+        ground: "#E2BF65",
+        flying: "#A98FF3",
+        psychic: "#F95587",
+        bug: "#A6B91A",
+        rock: "#B6A136",
+        ghost: "#735797",
+        dragon: "#6F35FC",
+        dark: "#705746",
+        steel: "#B7B7CE",
+        fairy: "#D685AD",
+      };
+
+      let currentTypes = [];
+      let currentColors = [];
+      pokemonData["types"] = pokemonData["types"].sort((a, b) => a["slot"] - b["slot"]);
+      pokemonData["types"].map((type) => {
+        currentTypes.push(type["type"]["name"]);
+        currentColors.push(typeColors[type["type"]["name"]]);
+      });
+
+      console.log(currentTypes);
+      console.log(currentColors);
+
+      if (currentColors.length === 1) {
+        typeDiv = (
+        <div className="pokemon-type" style={{background: currentColors[0]}}>
+          <p className="pokemon-type-text">{currentTypes[0]}</p>
+        </div>);
+      } else if (currentColors.length === 2) {
+        typeDiv = (
+        <div className="pokemon-type" style={{background: `linear-gradient(45deg, ${currentColors[0]} 50%, ${currentColors[1]} 50%)`}}>
+          <p className="pokemon-type-text">{currentTypes[0]} - {currentTypes[1]}</p>
+        </div>
+        );
+      }
+    }
+    /*---------------*/
+    /*TYPE CODE*/
+    /*---------------*/
+
     if (bShowData) { singleBodyClassName += " w-100"};
     return (
       <div className={singleBodyClassName} onClick={() => this.handleClick()}>
@@ -173,6 +229,7 @@ class Pokemon extends Component {
         </div>
 
         <Collapse isOpened={this.state.bShowData} springConfig={presets.wobbly}>
+          {typeDiv}
           {pokemonSingleBody}
         </Collapse>
       </div>
@@ -234,5 +291,29 @@ class Pokemon extends Component {
     }
   }
 }
+
+
+/*
+  background: linear-gradient(45deg, #7AC74C 50%, #A33EA1 50%);
+  Type colors:
+  Normal Type: A8A77A
+  Fire Type:  EE8130
+  Water Type:  6390F0
+  Electric Type:  F7D02C
+  Grass Type:  7AC74C
+  Ice Type:  96D9D6
+  Fighting Type:  C22E28
+  Poison Type:  A33EA1
+  Ground Type:  E2BF65
+  Flying Type:  A98FF3
+  Psychic Type:  F95587
+  Bug Type:  A6B91A
+  Rock Type:  B6A136
+  Ghost Type:  735797
+  Dragon Type:  6F35FC
+  Dark Type:  705746
+  Steel Type:  B7B7CE
+  Fairy Type:  D685AD
+*/
 
 export default Pokemon;
